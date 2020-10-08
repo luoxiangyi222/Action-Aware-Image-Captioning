d2534d3e6b9a735bc9f175fee9de5a86f256261f
xiangyi <luoxiangyi222@gmail.com>
2020-10-08 20:10:08 +1100

    add new file: code_to_vec

---
+++ code_to_vec.py
@@ -0,0 +1,9 @@
+"""
+Author: Xinagyi Luo
+
+This module runs  Word2Vec to learn word embeddings for code corpus.
+
+"""
+
+from gensim.test.utils import common_texts, get_tmpfile
+from gensim.models import Word2Vec

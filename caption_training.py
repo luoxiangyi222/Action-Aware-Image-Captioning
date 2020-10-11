6bc5ba9f0a67e74affec08862d8977299de8cae6
Xiangyi  Luo <u6162693@anu.edu.au>
2020-10-11 19:22:52 +1100

    save captions

--- caption_training.py
+++ caption_training.py
@@ -248,12 +248,24 @@ def evaluate(image):
     return result, attention_plot
 
 
+real_file_path = 'real_caption.txt'
+pred_file_path = 'pred_caption.txt'
+
+real_f = open(real_file_path, 'w')
+pred_f = open(pred_file_path, 'w')
+
 # captions on the validation set
 for i, val_x in enumerate(val_X):
 
     val_y = np.array(tf.gather(val_Y, i))
     real_caption = ' '.join([tokenizer.index_word[j] for j in val_y if j not in [0]])
-    result, _ = evaluate(val_x)
+    pred_caption, _ = evaluate(val_x)
+
+    real_f.write(real_caption)
+    pred_f.write(pred_caption)
     print('//////////////////////////////////////////////////')
     print('Real Caption:', real_caption)
-    print('Prediction Caption:', ' '.join(result))
+    print('Prediction Caption:', ' '.join(pred_caption))
+
+real_f.close()
+pred_f.close()
